6742fe3f852eab342cb82625218a4a8599f4a455
Aivanii <aivanellok@gmail.com>
2025-05-17 17:15:06 +0700

    updated main screeen

--- piercie/src/App.tsx
+++ piercie/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./widgets/header/header";
+import MainScreen from "./widgets/mainScreen.tsx/mainScreen";
 
 function App() {
   return (
     <>
       <Header />
+      <MainScreen />
       <p className="bg-yellow-400 text-9xl">hello</p>
     </>
   );
---
+++ piercie/src/widgets/mainScreen.tsx/mainScreen.tsx
@@ -0,0 +1,22 @@
+const MainScreen = () => {
+  return (
+    <div>
+      <div>
+        <img
+          src="./young-woman-doing-piercing-at-beauty-studio-salon.jpeg"
+          alt="background"
+        />
+      </div>
+      <div>
+        <h1>Advanced Piercing Techniques Available</h1>
+        <p>
+          Experience cutting-edge piercing techniques at Piercie's, your London
+          destination for modern body art excellence and safety.
+        </p>
+        <button>Explore piercing options</button>
+      </div>
+    </div>
+  );
+};
+
+export default MainScreen;
