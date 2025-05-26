67c211ccc9440b59e261dd7c3c4d5164acd101ad
Aivanii <aivanellok@gmail.com>
2025-05-26 09:39:22 +0700

    added styles for companiesScroll

--- src/widgets/companiesScroll/companiesScroll.tsx
+++ src/widgets/companiesScroll/companiesScroll.tsx
@@ -1,7 +1,64 @@
+const companiesLogos = [
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
+    alt: "Company X logo",
+  },
+];
+
 const CompaniesScroll = () => {
-    return(<div>
-        hello
-    </div>)
-}
+  return (
+    <div
+      className="h-full max-w-[1920px] m-auto
+                flex w-full overflow-hidden relative
+                py-20 gap-20"
+    >
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        <div className="absolute left-0 top-0 h-full w-[120px] z-10 shadow-[inset_120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
+        <div className="absolute right-0 top-0 h-full w-40 z-10 shadow-[inset_-120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
+      </div>
+      {companiesLogos.map((elem) => (
+        <div key={elem.src} className="relative h-full flex-shrink-0">
+          <img
+            className="relative h-full object-contain"
+            src={elem.src}
+            alt={elem.alt}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default CompaniesScroll;
