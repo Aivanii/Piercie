cc92dde58e76bbdabc23eb4fbcaa1c15e7fb9797
Aivanii <aivanellok@gmail.com>
2025-05-26 10:40:34 +0700

    updated styles for ourSkills container

--- src/widgets/ourSkills/ourSkills.tsx
+++ src/widgets/ourSkills/ourSkills.tsx
@@ -1,9 +1,78 @@
 const OurSkills = () => {
-    return(
-        <div>
-            our skills
+  return (
+    <div
+      className="bg-[#232321] w-dvw max-w-[1920px] m-auto
+      flex items-center justify-center flex-col"
+    >
+      <div
+        className="flex justify-center items-center flex-col
+        w-full p-12 pt-40"
+      >
+        <h2 className="font-bold text-4xl jost-styles text-white w-full text-center">
+          Advanced Earings Crafting
+        </h2>
+        <div className="flex mt-6 gap-2 md:gap-4 lg:gap-8">
+          <div className="relative">
+            <img
+              className="h-144 w-100 object-cover relative"
+              src="https://media.gettyimages.com/id/1500832940/photo/pharmacist-uses-a-specialized-piercing-gun-to-create-a-new-earlobe-piercing.jpg?b=1&s=2048x2048&w=0&k=20&c=zQxj1pAFEc2SPNaD3Oq-WwJ8JUXT3Xj5SdFZGrcq4wE="
+              alt="Pharmacist uses a specialized piercing gun to create a new earlobe piercing"
+            />
+            <div
+              className="absolute w-full bottom-0 -transform-y-[50%]
+            bg-[#000000b5] p-6"
+            >
+              <p className="jost-styles text-3xl font-bold text-white">
+                Custom Piercing Consultation
+              </p>
+              <p className="jost-styles text-white font-light mt-2">
+                Receive personalized advice and design ideas to achieve the
+                perfect piercing tailored for you.
+              </p>
+            </div>
+          </div>
+          <div className="relative">
+            <img
+              className="h-144 w-100 object-cover relative"
+              src="https://media.gettyimages.com/id/1500832940/photo/pharmacist-uses-a-specialized-piercing-gun-to-create-a-new-earlobe-piercing.jpg?b=1&s=2048x2048&w=0&k=20&c=zQxj1pAFEc2SPNaD3Oq-WwJ8JUXT3Xj5SdFZGrcq4wE="
+              alt="Pharmacist uses a specialized piercing gun to create a new earlobe piercing"
+            />
+            <div
+              className="absolute w-full bottom-0 -transform-y-[50%]
+            bg-[#000000b5] p-6"
+            >
+              <p className="jost-styles text-3xl font-bold text-white">
+                Custom Piercing Consultation
+              </p>
+              <p className="jost-styles text-white font-light mt-2">
+                Receive personalized advice and design ideas to achieve the
+                perfect piercing tailored for you.
+              </p>
+            </div>
+          </div>
+          <div className="relative">
+            <img
+              className="h-144 w-100 object-cover relative"
+              src="https://media.gettyimages.com/id/1500832940/photo/pharmacist-uses-a-specialized-piercing-gun-to-create-a-new-earlobe-piercing.jpg?b=1&s=2048x2048&w=0&k=20&c=zQxj1pAFEc2SPNaD3Oq-WwJ8JUXT3Xj5SdFZGrcq4wE="
+              alt="Pharmacist uses a specialized piercing gun to create a new earlobe piercing"
+            />
+            <div
+              className="absolute w-full bottom-0 -transform-y-[50%]
+            bg-[#000000b5] p-6"
+            >
+              <p className="jost-styles text-3xl font-bold text-white">
+                Custom Piercing Consultation
+              </p>
+              <p className="jost-styles text-white font-light mt-2">
+                Receive personalized advice and design ideas to achieve the
+                perfect piercing tailored for you.
+              </p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default OurSkills;
