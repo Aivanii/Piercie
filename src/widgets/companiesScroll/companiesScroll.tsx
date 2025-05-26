59c6e0c17059a744541df67210570155e0a853a8
Aivanii <aivanellok@gmail.com>
2025-05-26 10:11:10 +0700

    added smooth scroll anim for companies scroll tsx

--- src/widgets/companiesScroll/companiesScroll.tsx
+++ src/widgets/companiesScroll/companiesScroll.tsx
@@ -1,39 +1,27 @@
 const companiesLogos = [
   {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-1.png",
+    alt: "Company X1 logo",
+  },
+  {
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-2.png",
+    alt: "Company X2 logo",
   },
   {
     src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
+    alt: "Company X3 logo",
   },
   {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-4.png",
+    alt: "Company X4 logo",
   },
   {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-5.png",
+    alt: "Company X5 logo",
   },
   {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
-  },
-  {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
-  },
-  {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
-  },
-  {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
-  },
-  {
-    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-3.png",
-    alt: "Company X logo",
+    src: "https://cdn.durable.co/assets/logo-showcase/logo-ipsum-6.png",
+    alt: "Company X6 logo",
   },
 ];
 
@@ -41,22 +29,38 @@ const CompaniesScroll = () => {
   return (
     <div
       className="h-full max-w-[1920px] m-auto
-                flex w-full overflow-hidden relative
-                py-20 gap-20"
+                flex w-full overflow-hidden"
     >
+      {/*
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
         <div className="absolute left-0 top-0 h-full w-[120px] z-10 shadow-[inset_120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
         <div className="absolute right-0 top-0 h-full w-40 z-10 shadow-[inset_-120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
+      </div>* */}
+      <div
+        id="scrollCompaniesElem"
+        className="h-full m-auto
+                flex w-full relative
+                py-20 gap-20 companies-scroll"
+      >
+        {companiesLogos.map((elem) => (
+          <div key={elem.src} className="relative h-full flex-shrink-0">
+            <img
+              className="relative h-full object-contain"
+              src={elem.src}
+              alt={elem.alt}
+            />
+          </div>
+        ))}
+        {companiesLogos.map((elem) => (
+          <div key={`elem.src${2}`} className="relative h-full flex-shrink-0">
+            <img
+              className="relative h-full object-contain"
+              src={elem.src}
+              alt={elem.alt}
+            />
+          </div>
+        ))}
       </div>
-      {companiesLogos.map((elem) => (
-        <div key={elem.src} className="relative h-full flex-shrink-0">
-          <img
-            className="relative h-full object-contain"
-            src={elem.src}
-            alt={elem.alt}
-          />
-        </div>
-      ))}
     </div>
   );
 };
