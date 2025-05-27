34bb7215e70806a680678d1bdb0411facaa199c2
Aivanii <aivanellok@gmail.com>
2025-05-27 10:34:31 +0700

    added an empty slider into files and imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import Header from "./widgets/header/header";
 import MainScreen from "./widgets/mainScreen.tsx/mainScreen";
 import CompaniesScroll from "./widgets/companiesScroll/companiesScroll";
 import OurSkills from "./widgets/ourSkills/ourSkills";
+import Slider from "./widgets/slider/slider";
 
 function App() {
   return (
     <>
       <Header />
       <MainScreen />
-      <CompaniesScroll/>
-    <OurSkills/>
+      <CompaniesScroll />
+      <OurSkills />
+      <Slider />
     </>
   );
 }
