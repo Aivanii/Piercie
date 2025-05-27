58c0f843784a9e542e300baf3d2318c7cea2c900
Aivanii <aivanellok@gmail.com>
2025-05-27 12:51:07 +0700

    added slider

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./widgets/header/header";
 import MainScreen from "./widgets/mainScreen.tsx/mainScreen";
 import CompaniesScroll from "./widgets/companiesScroll/companiesScroll";
 import OurSkills from "./widgets/ourSkills/ourSkills";
-import Slider from "./widgets/slider/slider";
+import SliderOurWorks from "./widgets/sliderOurWorks/sliderOurWorks";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <MainScreen />
       <CompaniesScroll />
       <OurSkills />
-      <Slider />
+      <SliderOurWorks />
     </>
   );
 }
