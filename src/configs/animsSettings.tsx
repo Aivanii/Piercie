2153f8c77edb5962e4fb8294d6b9823ae807b600
Aivanii <aivanellok@gmail.com>
2025-06-02 10:14:51 +0700

    added fade-in react-spring animation

---
+++ src/configs/animsSettings.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { config } from "@react-spring/web";
+
+export const FadeIn: React.FC<React.HTMLAttributes<HTMLDivElement>> = (
+  props
+) => {
+  const fadeInAnim = useSpring({
+    from: { y: 50, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+    config: config.slow,
+  });
+
+  return (
+    <animated.div {...props} style={{ ...fadeInAnim, ...props.style }}>
+      {props.children}
+    </animated.div>
+  );
+};
