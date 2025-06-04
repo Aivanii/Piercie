9c7e3ddff8c0513ab02be00513de068e57272c7a
Aivanii <aivanellok@gmail.com>
2025-06-04 22:01:40 +0700

    added logic for formSubmit in contacts

--- src/widgets/contacts/contacts.tsx
+++ src/widgets/contacts/contacts.tsx
@@ -1,6 +1,102 @@
+import { useRef, useState } from "react";
+
+interface formData {
+  name: string;
+  email: string;
+  message: string;
+}
+
 const Contacts = () => {
+  const [formData, setFormData] = useState<formData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const messageInputRef = useRef(null);
+
+  const nameInputErrorTxtRef = useRef(null);
+  const emailInputErrorTxtRef = useRef(null);
+  const messageInputErrorTxtRef = useRef(null);
+
+  const changeFormData = (event) => {
+    const { name, value } = event.target;
+    if (!name || !value) {
+      return;
+    }
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    clearIncorrectTexts();
+
+    let isFormValid: boolean = true;
+
+    if (!isNameValid()) {
+      isFormValid = false;
+      setIncorrectNameText();
+    }
+
+    if (!isEmailValid()) {
+      isFormValid = false;
+      setIncorrectEmailText();
+    }
+
+    if (!isMessageValid()) {
+      isFormValid = false;
+      setIncorrectMessageText();
+    }
+
+    if (isFormValid) {
+      submitValidatedForm();
+    }
+  };
+
+  const isNameValid = () => {
+    return formData.name.trim().length > 1;
+  };
+
+  const setIncorrectNameText = () => {
+    if (!nameInputErrorTxtRef?.current) return;
+    nameInputErrorTxtRef.current.textContent = "* Incorrect Name";
+  };
+
+  const isEmailValid = () => {
+    const email_regexp =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    return email_regexp.test(formData.email);
+  };
+
+  const setIncorrectEmailText = () => {
+    if (!emailInputErrorTxtRef?.current) return;
+    emailInputErrorTxtRef.current.textContent = "* Incorrect Email";
+  };
+
+  const isMessageValid = () => {
+    return formData.message.trim().length > 20;
+  };
+
+  const setIncorrectMessageText = () => {
+    if (!messageInputErrorTxtRef?.current) return;
+    messageInputErrorTxtRef.current.textContent = "* Incorrect Message";
+  };
+
+  const clearIncorrectTexts = () => {
+    nameInputErrorTxtRef.current.textContent = "";
+    emailInputErrorTxtRef.current.textContent = "";
+    messageInputErrorTxtRef.current.textContent = "";
+  }
+
+  const submitValidatedForm = () => {
+    alert("Form was successfully submited. Thank you!");
+    location.reload();
+  };
+
   return (
-    <section id = "contacts-section">
+    <section id="contacts-section">
       <div className="bg-[#22211F] py-10 mt-40 w-dvw max-w-[1920px] m-auto text-white">
         <div className="flex flex-wrap justify-around items-start">
           <div
@@ -15,14 +111,21 @@ const Contacts = () => {
               We're here to assist you.
             </p>
           </div>
-          <form className="w-1/3 grid grid-cols-2 gap-4">
+          <form
+            className="w-1/3 grid grid-cols-2 gap-4"
+            onSubmit={handleFormSubmit}
+          >
             <div>
               <label>Name</label>
               <input
                 type="text"
+                name="name"
                 autoComplete="name"
                 className="bg-[#2E2E2B] w-full p-2"
+                onChange={changeFormData}
+                ref={nameInputRef}
               />
+              <span ref={nameInputErrorTxtRef} className="text-red-600"></span>
             </div>
             <div>
               <label>Email</label>
@@ -30,11 +133,24 @@ const Contacts = () => {
                 type="text"
                 autoComplete="email"
                 className="bg-[#2E2E2B] w-full p-2"
+                name="email"
+                onChange={changeFormData}
+                ref={emailInputRef}
               />
+              <span ref={emailInputErrorTxtRef} className="text-red-600"></span>
             </div>
             <div className="col-span-2">
               <label>Message</label>
-              <textarea className="w-full min-h-48 bg-[#2E2E2B] p-2"></textarea>
+              <textarea
+                className="w-full min-h-48 bg-[#2E2E2B] p-2"
+                name="message"
+                onChange={changeFormData}
+                ref={messageInputRef}
+              />
+              <span
+                ref={messageInputErrorTxtRef}
+                className="text-red-600"
+              ></span>
             </div>
             <button
               className="py-4 bg-[#E5AF24] text-center hover:bg-[#D09F23] duration-150 cursor-pointer
