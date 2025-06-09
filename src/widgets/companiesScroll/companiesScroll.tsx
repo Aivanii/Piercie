d26f89a5aac2bb23fdbc12debc155092206ace34
Aivanii <aivanellok@gmail.com>
2025-06-09 07:13:02 +0700

    updated styles for companiesScroll responsiveness

--- src/widgets/companiesScroll/companiesScroll.tsx
+++ src/widgets/companiesScroll/companiesScroll.tsx
@@ -24,12 +24,12 @@ const companiesLogos = [
 const CompaniesScroll = () => {
   return (
     <div
-      className="h-full max-w-[1920px] m-auto
-                flex w-full relative"
+      className="h-full max-w-[1920px] w-dvw m-auto
+                flex relative overflow-hidden"
     >
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
         <div className="absolute left-0 top-0 h-full w-40 z-10 shadow-[inset_120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
-        <div className="absolute right-0 top-0 h-full w-40 z-10 shadow-[inset_-120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
+        <div className="absolute -right-1 top-0 h-full w-40 z-10 shadow-[inset_-120px_0_60px_-60px_rgba(255,255,255,1)]"></div>
       </div>
       <div
         id="scrollCompaniesElem"
